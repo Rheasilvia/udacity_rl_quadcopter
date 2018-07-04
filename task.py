36986099e314208413091ee449948ae1999ae9d8
chenmengjie <chenmengjie@xiaomi.com>
2018-07-04 21:55:15 +0800

    上传文件

--- task.py
+++ task.py
@@ -47,3 +47,57 @@ class Task():
         self.sim.reset()
         state = np.concatenate([self.sim.pose] * self.action_repeat) 
         return state
+
+
+class Launch():
+    def __init__(self, init_pose=None, init_velocities=None,
+        init_angle_velocities=None, runtime=5., target_pos=None):
+
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime)
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 0
+        self.action_high = 900
+        self.action_size = 4
+
+        # Goal
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
+        self.target_z = self.target_pos[2]
+
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        sim_z = self.sim.pose[2]
+        reward = -min(abs(self.target_z - sim_z), 20.0)
+        if sim_z > self.target_z:
+            reward += 10
+            self.sim.done = True
+
+        elif self.sim.time > self.sim.runtime:
+            reward -= 10
+            self.sim.done = True
+
+        reward = np.tanh(reward)
+        return(reward)
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        done = False
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
+            reward += self.get_reward()
+            pose_all.append(self.sim.pose)
+            # if done:
+                # reward += 10
+
+        next_state = np.concatenate(pose_all)
+        return(next_state, reward, done)
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat)
+        return(state)
